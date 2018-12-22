3afb7437921fa27540775d4949820b969f133ccb
DeexIsTheBest <45711286+DeexIsTheBest@users.noreply.github.com>
2018-12-22 10:12:15 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,35 +1,12 @@
 ﻿const Discord = require("discord.js");
 const client = new Discord.Client();
-var prefix = "$";
-client.on("message", message => {
-
-            if (message.content.startsWith(prefix + "bc")) {
-                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
-  let args = message.content.split(" ").slice(1);
-  var argresult = args.join(' '); 
-  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
- m.send(`${argresult}\n ${m}`);
-})
- message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
- message.delete(); 
-};     
+client.on('guildMemberAdd', member => {
+     var guild;
+    while (!guild)
+        guild = client.guilds.get("487670104646090754") 
+let channel = member.guild.channels.find('name', 'moments');
+  if (!channel) return; 
+client.channels.get("525749764474798087").send(`**. # Welcome To Moments Server:heart: :notes:** `) 
 });
-
-
-client.on('ready', () => {
-   console.log(`----------------`);
-      console.log(`Desert Bot- Script By : i1Suhaib`);
-        console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
-    console.log(`----------------`);
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`$bc | -Deèx``,"http://twitch.tv/S-F")
-client.user.setStatus("dnd")
-});
-
-
-
-
-
 
 client.login(process.env.BOT_TOKEN);
